79f9f6e5c11946491eddbd1f85c548ec2aaa0ddd
Olena Iliushyn <lenailyshun@gmail.com>
2026-04-20 20:13:09 +0300

    SMPT protocol

---
+++ 03_SMTP_Client/LoginWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _03_SMTP_Client
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public string Login { get; set; }
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login = loginTb.Text;   
+            MainWindow mainWindow = new MainWindow(Login);
+            //mainWindow.Show();
+            mainWindow.ShowDialog();
+
+            this.Hide();
+        }
+    }
+}
---
+++ 03_SMTP_Client/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace _03_SMTP_Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string server = "smtp.gmail.com";
+        int port = 587;
+        string username = "lenailyshun@gmail.com";
+        string password = "hwpmnsbvhaqkgfcd";
+        //03_SMTP_Client
+        public MainWindow()
+        {
+            InitializeComponent();
+            fromTb.Text = "lenailyshun@gmail.com";
+            toTb.Text = "teyes23644@mypethealh.com";
+        }
+        public MainWindow(string login)
+        {
+            InitializeComponent();
+            fromTb.Text = login;
+            toTb.Text = "teyes23644@mypethealh.com";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MailMessage message = new MailMessage(fromTb.Text,
+                toTb.Text, subjectTb.Text, messageTb.Text );
+
+            using (StreamReader sr = new StreamReader(@"Files/mail.html"))
+            {
+                message.Body = sr.ReadToEnd();  
+            }
+            message.IsBodyHtml = true;  
+            message.Priority = MailPriority.High;
+            message.Attachments.Add(new Attachment(@"Files/text.txt"));
+            message.Attachments.Add(new Attachment(@"Files/nuts.jpg"));
+
+            SmtpClient smtpClient = new SmtpClient(server,port);
+            smtpClient.EnableSsl = true;
+
+            smtpClient.Credentials = new NetworkCredential(username, password);
+
+            smtpClient.SendCompleted += SmtpClient_SendCompleted;
+            smtpClient.SendAsync(message, message);
+
+        }
+
+        private void SmtpClient_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            var state = e.UserState as MailMessage;
+            MessageBox.Show($"Message was send ! {state?.Subject}");
+        }
+    }
+}
